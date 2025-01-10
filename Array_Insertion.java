7862cd6d85511a351136e934430814e3da773dc5
1U23IT016CHANDRU <139447715+2023ITACHANDRU@users.noreply.github.com>
2025-01-10 08:33:39 +0000

    Done4

--- Array_Insertion.java
+++ Array_Insertion.java
@@ -28,16 +28,16 @@ public class Array_Insertion {
 
     static void arrayInsert(int arr[], int p, int e, int len) {
         for (int i = len - 1; i >= p - 1; i--) {
-            arr[i + 1] = arr[i];
+            arr[i + 1]=arr[i];
         }
 
-        arr[p - 1] = e;
+        arr[p - 1]=e;
 
         System.out.println("After Inserting");
-        System.out.println("Length of the array: " + (len + 1));
+        System.out.println("Length of the array:" + (len + 1));
 
         for (int i = 0; i < len + 1; i++) {
-            System.out.println("arr[" + i + "] = " + arr[i]);
+            System.out.println("arr[" + i + "]=" + arr[i]);
         }
     }
 }
