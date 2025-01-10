51d3a99c79078daba5fa78a4927657415d058d59
1U23IT016CHANDRU <139447715+2023ITACHANDRU@users.noreply.github.com>
2025-01-10 08:24:26 +0000

    Done4

--- Array_Insertion.java
+++ Array_Insertion.java
@@ -1,29 +1,23 @@
+
 import java.util.Scanner;
 
 public class Array_Insertion {
-
     public static void main(String[] args) {
-
         int len, p, e;
-
         System.out.println("ARRAY OPERATION");
-
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter the length of the Array");
         len = sc.nextInt();
-
         int arr[] = new int[len + 1];
 
         System.out.println("Enter " + len + " element(s) to insert");
-
         for (int i = 0; i < len; i++) {
             arr[i] = sc.nextInt();
         }
 
         System.out.println("Enter the position where you want to insert:");
         p = sc.nextInt();
-
         System.out.println("Enter the element to be inserted");
         e = sc.nextInt();
 
@@ -36,14 +30,57 @@ public class Array_Insertion {
         for (int i = len - 1; i >= p - 1; i--) {
             arr[i + 1] = arr[i];
         }
+
         arr[p - 1] = e;
 
-        System.out.println("Updated array after insertion:");
-        for (int i = 0; i <= len; i++) {
-            System.out.print(arr[i] + " ");
-        }
+        System.out.println("After Inserting");
+        System.out.println("Length of the array: " + (len + 1));
 
-        System.out.println("\nUpdated length of the array: " + (len + 1));
+        for (int i = 0; i < len + 1; i++) {
+            System.out.println("arr[" + i + "] = " + arr[i]);
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
